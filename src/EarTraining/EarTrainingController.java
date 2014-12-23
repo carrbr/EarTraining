84c4fc7796e4194480d93e1a579684c1a131dcd4
panderingPenguin <brn.carr141@gmail.com>
2014-12-23 17:59:59 -0500

    Preliminary GUI built

--- src/EarTraining/EarTrainingController.java
+++ src/EarTraining/EarTrainingController.java
@@ -1,4 +1,23 @@
-package sample;
+package EarTraining;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class EarTrainingController {
+    @FXML private Text actiontarget;
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+        System.out.println("Button Pressed");
+    }
+
+    public void playRandomNote(ActionEvent actionEvent) {
+    }
+
+    public void replayNote(ActionEvent actionEvent) {
+    }
+
+    public void submitGuess(ActionEvent actionEvent) {
+    }
 }
--- src/EarTraining/EarTrainingView.java
+++ src/EarTraining/EarTrainingView.java
@@ -1,9 +1,12 @@
-package sample;
+package EarTraining;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class EarTrainingView extends Application {
@@ -13,6 +16,7 @@ public class EarTrainingView extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("EarTrainingMain.fxml"));
         primaryStage.setTitle("Ear Training");
         primaryStage.setScene(new Scene(root, 800, 550));
+
         primaryStage.show();
     }
 
